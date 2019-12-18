90e427747e3bb72c0b363a85df6ef4a506c0ea14
James Goode <james_goode@hotmail.co.uk>
2019-12-18 23:45:48 +0000

    Started player details form

---
+++ LtBookingSite/Models/PlayerDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LtBookingSite.Models
+{
+    public class PlayerDetailsViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        public string EmergencyContactName { get; set; }
+
+        [Required]
+        [Phone]
+        public string EmergencyContactPhoneNumber { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
